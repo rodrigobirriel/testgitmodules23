5674ec2fb115d2143f014e2df3aab42c06d852f4
Rodrigo Birriel <rbirriel@altimetrik.com>
2015-02-25 10:21:06 -0200

    commit with code

---
+++ testingmodules23/testingmodules23/testingmodules23.h
@@ -0,0 +1,13 @@
+//
+//  testingmodules23.h
+//  testingmodules23
+//
+//  Created by Rodrigo Birriel on 2/25/15.
+//  Copyright (c) 2015 Altimetrik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testingmodules23 : NSObject
+
+@end
---
+++ testingmodules23/testingmodules23/testingmodules23.m
@@ -0,0 +1,13 @@
+//
+//  testingmodules23.m
+//  testingmodules23
+//
+//  Created by Rodrigo Birriel on 2/25/15.
+//  Copyright (c) 2015 Altimetrik. All rights reserved.
+//
+
+#import "testingmodules23.h"
+
+@implementation testingmodules23
+
+@end
